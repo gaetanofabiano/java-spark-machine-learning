5478d3bd20df857dbc3f18b134992db436af6f60
gaetanofabiano <fabiano.gaetano@gmail.com>
2020-07-14 09:36:51 +0200

    updated

--- src/main/java/it/fabiano/bigdata/spark/ml/es00/Matrices.java
+++ src/main/java/it/fabiano/bigdata/spark/ml/es00/Matrices.java
@@ -12,11 +12,12 @@ import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.distributed.RowMatrix;
 
 /**
-* Java-Spark-Training-Course
+* Java-Spark-Machine-Learning-Course
 *
 * @author  Gaetano Fabiano
-* @version 1.0.0
+* @version 1.1.0
 * @since   2019-07-19 
+* @updated 2020-07-01 
 */
 
 public class Matrices {
